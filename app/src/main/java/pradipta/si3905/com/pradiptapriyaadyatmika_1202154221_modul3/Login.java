74021cde6c20218607a1dfae3f1f15b267c89de0
Pradipta Priya Adyatmika <pradiptapaa@gmail.com>
2018-02-24 07:21:32 +0700

    Test using git with command line

---
+++ app/src/main/java/pradipta/si3905/com/pradiptapriyaadyatmika_1202154221_modul3/Login.java
@@ -0,0 +1,13 @@
+package pradipta.si3905.com.pradiptapriyaadyatmika_1202154221_modul3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/pradipta/si3905/com/pradiptapriyaadyatmika_1202154221_modul3/SplashScreen.java
@@ -0,0 +1,35 @@
+package pradipta.si3905.com.pradiptapriyaadyatmika_1202154221_modul3;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity {
+
+int splashDelay = 1500;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        //buat handler untuk post delayed
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent pindahactivity = new Intent(getApplicationContext(), Login.class);
+                startActivity(pindahactivity);
+
+                finish();
+            }
+        }, splashDelay );
+
+    }
+
+
+}
